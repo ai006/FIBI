91307dc5ed7a7fc818f26036ab8922467fcd29f9
Ange-Thierry Ishimwe <ai006@uark.edu>
2019-09-27 08:40:22 -0800

    User Add job : done
    finished with the user adds jobs page. next going to work with server to accept jobs

--- userClient/api.js
+++ userClient/api.js
@@ -5,6 +5,7 @@ import {fetchJobsPending, fetchJobsError, fetchJobSuccess} from './actions/index
 const API_URL = 'http://10.0.2.2:3001/api/getData';
 
 
+//function used to call and fetch the jobs data from the server
 export const fetchJobsData =  () => {
     return async (dispatch) => {
       try{
@@ -21,3 +22,12 @@ export const fetchJobsData =  () => {
       }
     }
 }
+
+
+  //function used to send jobs that are used by user to server
+  export const sendAddedJob = (data) => {
+    console.log("jobs to send\n\n");
+    console.log(data);
+  }
+
+
