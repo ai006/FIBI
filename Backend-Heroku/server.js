10832f8e3a09636d9c3e132ffe61f6269330e426
ai006 <35574362+ai006@users.noreply.github.com>
2020-09-05 00:18:32 -0600

    cleaned files
    cleaned all the files

--- Backend-Heroku/server.js
+++ Backend-Heroku/server.js
@@ -20,7 +20,7 @@ app.use(cors());
 
 // this is our MongoDB database
 const dbRoute =
-  'mongodb+srv://ai006:Qwerty1234@wesponsorapp-8krvn.mongodb.net/test?retryWrites=true&w=majority';
+  'mongodb+srv://guest:guest123@wesponsorapp-8krvn.mongodb.net/test?retryWrites=true&w=majority';
 
 // connects our back end code with the database
 //mongoose.connect(dbRoute, { useNewUrlParser: true },{useUnifiedTopology: true});
